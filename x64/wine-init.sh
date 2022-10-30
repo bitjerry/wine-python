f97440a68616a4bdfae580214cd0c46faa45cde8
Mr.lin <adobe@outlook.my>
2022-10-30 10:45:11 +0800

    v1.2.0

---
+++ x64/wine-init.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Prepare environment:
+# - Set Windows version to 10
+# - Disable menu updates
+# - Disable Mono
+# - Disable Gecko
+set -e
+
+export WINEDLLOVERRIDES="winemenubuilder.exe,mscoree,mshtml="
+wine reg add 'HKLM\Software\Microsoft\Windows NT\CurrentVersion' /v CurrentVersion /d 10.0 /f
+wine reg add 'HKCU\Software\Wine\DllOverrides' /v winemenubuilder.exe /t REG_SZ /d '' /f
+wine reg add 'HKCU\Software\Wine\DllOverrides' /v mscoree /t REG_SZ /d '' /f
+wine reg add 'HKCU\Software\Wine\DllOverrides' /v mshtml /t REG_SZ /d '' /f
+wineserver -w
+
